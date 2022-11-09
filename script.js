5e77f37de2f65dc1660ebcfa0eb5fa20c1afde6b
nnapolitx <nnapolitano88@gmail.com>
2022-11-09 11:47:52 -0300

    made gameboard with IIFE, added eventlisteners to each square

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const gameboard = []
 const boardWrap = document.querySelector('.board-wrap')
 
+//add a IIFE for creating gameboard
+const makeGameBoard = (() => {
+    if (gameboard.length === 0) {
+        for (let i=0; i<9; i++) {
+            const space = document.createElement('div')
+            boardWrap.appendChild(space)
+            gameboard.push(space)
+            space.addEventListener('click', spaceClicked)
+        }
+    }
+})();
 
-//add a IFFE for creating gameboard
+function spaceClicked (e) {
+    console.log('click', e.target)
+}
+
 
 //add factory for two players
 
